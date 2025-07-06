2d34ff8b424d9a2640518649f9f22f46467b284c
SPinigin <pinigin@live.com>
2025-07-06 15:37:12 +0500

    add viewmodel

---
+++ OnlineTutor2/Models/ViewModels/StudentAllTestsIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OnlineTutor2.Models;
+
+namespace OnlineTutor2.ViewModels
+{
+    public class StudentAllTestsIndexViewModel
+    {
+        public Student Student { get; set; } = null!;
+        public IEnumerable<SpellingTest> SpellingTests { get; set; } = new List<SpellingTest>();
+        public IEnumerable<PunctuationTest> PunctuationTests { get; set; } = new List<PunctuationTest>();
+    }
+}
