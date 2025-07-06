6fba260fd6e0955cbd95a24c8c71ae263e52b13b
SPinigin <pinigin@live.com>
2025-07-06 16:29:05 +0500

    divide test types

---
+++ OnlineTutor2/Models/ViewModels/StudentTestHistoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OnlineTutor2.Models;
+
+namespace OnlineTutor2.ViewModels
+{
+    public class StudentTestHistoryViewModel
+    {
+        public Student Student { get; set; } = null!;
+        public IEnumerable<SpellingTestResult> SpellingResults { get; set; } = new List<SpellingTestResult>();
+        public IEnumerable<PunctuationTestResult> PunctuationResults { get; set; } = new List<PunctuationTestResult>();
+    }
+}
