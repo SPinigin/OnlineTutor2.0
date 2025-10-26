078d1cfbc09cb61b307d69475146f8de1db87875
SPinigin <pinigin@live.com>
2025-10-27 03:26:13 +0500

    add ortho viewmodels

---
+++ OnlineTutor2/Models/ViewModels/OrthoeopyTestAnalyticsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using OnlineTutor2.Models;
+
+namespace OnlineTutor2.ViewModels
+{
+    public class OrthoeopyTestAnalyticsViewModel
+    {
+        public OrthoeopyTest Test { get; set; } = null!;
+        public TestStatistics Statistics { get; set; } = new();
+        public List<OrthoeopyStudentResultViewModel> StudentResults { get; set; } = new();
+        public List<OrthoeopyQuestionAnalyticsViewModel> QuestionAnalytics { get; set; } = new();
+        public List<Student> StudentsNotTaken { get; set; } = new();
+    }
+
+    public class OrthoeopyStudentResultViewModel
+    {
+        public Student Student { get; set; } = null!;
+        public List<OrthoeopyTestResult> Results { get; set; } = new();
+        public int AttemptsUsed { get; set; }
+        public bool HasCompleted { get; set; }
+        public bool IsInProgress { get; set; }
+        public OrthoeopyTestResult? BestResult { get; set; }
+        public OrthoeopyTestResult? LatestResult { get; set; }
+        public TimeSpan? TotalTimeSpent { get; set; }
+    }
+
+    public class OrthoeopyQuestionAnalyticsViewModel
+    {
+        public OrthoeopyQuestion Question { get; set; } = null!;
+        public int TotalAnswers { get; set; }
+        public int CorrectAnswers { get; set; }
+        public int IncorrectAnswers { get; set; }
+        public double SuccessRate { get; set; }
+        public bool IsMostDifficult { get; set; }
+        public bool IsEasiest { get; set; }
+        public List<StressPositionMistakeViewModel> CommonMistakes { get; set; } = new();
+    }
+
+    public class StressPositionMistakeViewModel
+    {
+        public int IncorrectPosition { get; set; }
+        public int Count { get; set; }
+        public double Percentage { get; set; }
+        public List<string> StudentNames { get; set; } = new();
+    }
+}
---
+++ OnlineTutor2/Models/ViewModels/TakeOrthoeopyTestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OnlineTutor2.Models;
+
+namespace OnlineTutor2.ViewModels
+{
+    public class TakeOrthoeopyTestViewModel
+    {
+        public OrthoeopyTestResult TestResult { get; set; } = null!;
+        public TimeSpan TimeRemaining { get; set; }
+        public int CurrentQuestionIndex { get; set; }
+    }
+}
