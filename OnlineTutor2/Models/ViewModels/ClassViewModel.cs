79d97ea132d0a8880019cc5148c35bfdf2bbdd86
SPinigin <pinigin@live.com>
2025-10-28 20:47:03 +0500

    refactor

--- OnlineTutor2/Models/ViewModels/ClassViewModel.cs
+++ OnlineTutor2/Models/ViewModels/ClassViewModel.cs
@@ -2,6 +2,18 @@
 
 namespace OnlineTutor2.ViewModels
 {
+    public class CreateClassViewModel
+    {
+        [Required(ErrorMessage = "Название класса обязательно")]
+        [StringLength(100, ErrorMessage = "Название не должно превышать 100 символов")]
+        [Display(Name = "Название класса")]
+        public string Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Описание не должно превышать 500 символов")]
+        [Display(Name = "Описание")]
+        public string? Description { get; set; }
+    }
+
     public class EditClassViewModel
     {
         public int Id { get; set; }
