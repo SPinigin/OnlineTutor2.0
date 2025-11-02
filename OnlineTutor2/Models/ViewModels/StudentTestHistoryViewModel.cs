909475d603b753ef058132ed2eab3a7f4b9432ad
SPinigin <pinigin@live.com>
2025-11-02 23:28:45 +0500

    refactor

--- OnlineTutor2/Models/ViewModels/StudentTestHistoryViewModel.cs
+++ OnlineTutor2/Models/ViewModels/StudentTestHistoryViewModel.cs
@@ -8,5 +8,6 @@ namespace OnlineTutor2.ViewModels
         public IEnumerable<SpellingTestResult> SpellingResults { get; set; } = new List<SpellingTestResult>();
         public IEnumerable<PunctuationTestResult> PunctuationResults { get; set; } = new List<PunctuationTestResult>();
         public IEnumerable<OrthoeopyTestResult> OrthoeopyResults { get; set; } = new List<OrthoeopyTestResult>();
+        public IEnumerable<RegularTestResult> RegularResults { get; set; } = new List<RegularTestResult>();
     }
 }
