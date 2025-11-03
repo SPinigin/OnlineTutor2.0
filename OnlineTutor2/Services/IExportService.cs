d013d054b965bd7049a46a22c2675bbe897a81c5
SPinigin <pinigin@live.com>
2025-11-03 16:37:51 +0500

    add IExportService.cs

---
+++ OnlineTutor2/Services/IExportService.cs
@@ -0,0 +1,28 @@
+﻿namespace OnlineTutor2.Services
+{
+    public interface IExportService
+    {
+        Task<byte[]> ExportUsersToExcelAsync();
+        Task<byte[]> ExportUsersToCSVAsync();
+
+        Task<byte[]> ExportTeachersToExcelAsync();
+        Task<byte[]> ExportTeachersToCSVAsync();
+
+        Task<byte[]> ExportStudentsToExcelAsync();
+        Task<byte[]> ExportStudentsToCSVAsync();
+
+        Task<byte[]> ExportClassesToExcelAsync();
+        Task<byte[]> ExportClassesToCSVAsync();
+
+        Task<byte[]> ExportTestsToExcelAsync();
+        Task<byte[]> ExportTestsToCSVAsync();
+
+        Task<byte[]> ExportTestResultsToExcelAsync();
+        Task<byte[]> ExportTestResultsToCSVAsync();
+
+        Task<byte[]> ExportAuditLogsToExcelAsync();
+        Task<byte[]> ExportAuditLogsToCSVAsync();
+
+        Task<byte[]> ExportFullSystemToExcelAsync();
+    }
+}
