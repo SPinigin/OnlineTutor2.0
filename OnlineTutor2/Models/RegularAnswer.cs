6906aa0998594e0b2e652710dba493d6c93bc239
SPinigin <pinigin@live.com>
2025-11-03 17:21:07 +0500

    fix RegularAnswer model

--- OnlineTutor2/Models/RegularAnswer.cs
+++ OnlineTutor2/Models/RegularAnswer.cs
@@ -1,23 +1,28 @@
-﻿namespace OnlineTutor2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineTutor2.Models
 {
     public class RegularAnswer
     {
         public int Id { get; set; }
 
         public int TestResultId { get; set; }
-
         public int QuestionId { get; set; }
 
-        public int? AnswerId { get; set; }
+        // Для Single/Multiple Choice - ID выбранных вариантов
+        [StringLength(200)]
+        public string? SelectedOptionIds { get; set; }
 
-        public int? StudentAnswer { get; set; }
+        // Для текстовых ответов (если будут)
+        [StringLength(2000)]
+        public string? StudentAnswer { get; set; }
 
         public bool IsCorrect { get; set; }
-
         public int Points { get; set; }
+        public DateTime AnsweredAt { get; set; } = DateTime.Now;
 
         // Навигационные свойства
-        public virtual RegularTestResult RegularTestResult { get; set; }
-        public virtual RegularQuestion RegularQuestion { get; set; }
+        public virtual RegularTestResult RegularTestResult { get; set; } = null!;
+        public virtual RegularQuestion RegularQuestion { get; set; } = null!;
     }
 }
