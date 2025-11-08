a0ac39152cab467b425eeef81cb71190c971145a
SPinigin <pinigin@live.com>
2025-11-08 22:56:10 +0500

    add ProfileViewModel

---
+++ OnlineTutor2/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿using OnlineTutor2.Models;
+
+namespace OnlineTutor2.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public ApplicationUser User { get; set; }
+
+        // Статистика для студентов
+        public int TotalTestsCompleted { get; set; }
+        public double AverageScore { get; set; }
+        public int TotalPointsEarned { get; set; }
+
+        // Статистика для учителей
+        public int TotalStudents { get; set; }
+        public int TotalTests { get; set; }
+        public int TotalClasses { get; set; }
+    }
+}
