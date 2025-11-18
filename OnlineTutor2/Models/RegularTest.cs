4d1b19134fcb6a44abdcc721ad2ded5161fe3c86
SPinigin <pinigin@live.com>
2025-11-18 23:31:23 +0500

    fix RegularTest model (M-M)

--- OnlineTutor2/Models/RegularTest.cs
+++ OnlineTutor2/Models/RegularTest.cs
@@ -17,7 +17,6 @@ namespace OnlineTutor2.Models
         // Внешние ключи
         public string TeacherId { get; set; } = string.Empty;
         public int TestCategoryId { get; set; }
-        public int? ClassId { get; set; }
 
         // Настройки теста
         [Range(5, 300, ErrorMessage = "Время должно быть от 5 до 300 минут")]
@@ -47,7 +46,6 @@ namespace OnlineTutor2.Models
         // Навигационные свойства
         public virtual ApplicationUser Teacher { get; set; }
         public virtual TestCategory TestCategory { get; set; } = null!;
-        public virtual Class? Class { get; set; }
         public virtual ICollection<RegularQuestion> RegularQuestions { get; set; } = new List<RegularQuestion>();
         public virtual ICollection<RegularTestResult> RegularTestResults { get; set; } = new List<RegularTestResult>();
     }
