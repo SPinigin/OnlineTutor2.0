a4b05ee32f9d78b393db209363c77ab545418d75
SPinigin <pinigin@live.com>
2025-11-18 23:28:04 +0500

    fix many-to-many link for test, add ClassTestLink model

---
+++ OnlineTutor2/Models/ClassTestLinks.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineTutor2.Models
+{
+    // Связь SpellingTest <-> Class
+    public class SpellingTestClass
+    {
+        public int SpellingTestId { get; set; }
+        public int ClassId { get; set; }
+        public DateTime AssignedAt { get; set; } = DateTime.Now;
+
+        public virtual SpellingTest SpellingTest { get; set; } = null!;
+        public virtual Class Class { get; set; } = null!;
+    }
+
+    // Связь PunctuationTest <-> Class
+    public class PunctuationTestClass
+    {
+        public int PunctuationTestId { get; set; }
+        public int ClassId { get; set; }
+        public DateTime AssignedAt { get; set; } = DateTime.Now;
+
+        public virtual PunctuationTest PunctuationTest { get; set; } = null!;
+        public virtual Class Class { get; set; } = null!;
+    }
+
+    // Связь OrthoeopyTest <-> Class
+    public class OrthoeopyTestClass
+    {
+        public int OrthoeopyTestId { get; set; }
+        public int ClassId { get; set; }
+        public DateTime AssignedAt { get; set; } = DateTime.Now;
+
+        public virtual OrthoeopyTest OrthoeopyTest { get; set; } = null!;
+        public virtual Class Class { get; set; } = null!;
+    }
+
+    // Связь RegularTest <-> Class
+    public class RegularTestClass
+    {
+        public int RegularTestId { get; set; }
+        public int ClassId { get; set; }
+        public DateTime AssignedAt { get; set; } = DateTime.Now;
+
+        public virtual RegularTest RegularTest { get; set; } = null!;
+        public virtual Class Class { get; set; } = null!;
+    }
+}
